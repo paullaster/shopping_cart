0c358e4bec3ebec211dd98b653834ae18e87f5b2
paullaster <paullasterokoth98@gmail.com>
2022-09-08 12:18:55 +0300

    commit html file

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * SHOPPING CART
+ * 
+ * Cart is an object that have multiple or various instances
+ */
+//CART CLASS
+class ShoppingCart{
+    constructor(image, name, quantity, price, remove){
+        this.image = image;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.remove = remove;
+    }
+}
