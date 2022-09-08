1db274ae9c70b84ecab4937ca8f0a5bb9788440b
paullaster <paullasterokoth98@gmail.com>
2022-09-08 18:11:17 +0300

    Selecting click item

--- index.js
+++ index.js
@@ -26,7 +26,22 @@ let buyBtn = document.querySelectorAll('.add-to_cart-btn');
 /**
  *Converting Nodelist to Array 
  */
-
+let productArray = Array.from(buyBtn);
+for(let item of productArray){ 
+    item.addEventListener('click', function(e){
+        let productBtnDiv = e.target.parentElement;
+        let productContainerDiv = productBtnDiv.parentElement;
+        let productImage = productContainerDiv.querySelector('.products-image').src;
+        let productDetails = productContainerDiv.querySelector('.title').innerHTML;
+        console.log(productDetails);
+    })
+}
+/**
+ * ON BUY BUTTON CLICK EVENT
+ */
+function onItemClick(item) {
+    alert('You clicked me!')
+}
 function addToCart(){
 
 }
