5d3a939ec092227dc5866446fe7f7e6ba5c32ed1
paullaster <paullasterokoth98@gmail.com>
2022-09-09 09:10:30 +0300

    cart changes

--- index.js
+++ index.js
@@ -4,16 +4,27 @@
  * Cart is an object that have multiple or various instances
  */
 //CART CLASS
+
+let itemImg = document.createElement('img');
 class ShoppingCart{
-    constructor(image, name, quantity, price, remove){
+    constructor({image, title, quantity, price, remove}){
         this.image = image;
-        this.name = name;
+        this.title = title;
         this.quantity = quantity;
         this.price = price;
         this.remove = remove;
-    }
-    static displayCart(...cartInstances){
-        return `Cart Instance Cart Items stay here`;
+    } 
+    static displayCart(cartInstances){
+       /* let cartElement = {
+            image:cartInstances.image,
+            title: cartInstances.title ,
+            qunatity:cartInstances.quantity,
+            price: cartInstances.price,
+            remove: cartInstances.remove
+        };*/
+       // itemImg.setAttribute('src', cartElement.image);
+        //console.log(cartElement);
+        return 'We must od this';
     }
 }
 
@@ -27,21 +38,47 @@ let buyBtn = document.querySelectorAll('.add-to_cart-btn');
  *Converting Nodelist to Array 
  */
 let productArray = Array.from(buyBtn);
-for(let item of productArray){ 
-    item.addEventListener('click', function(e){
-        let productBtnDiv = e.target.parentElement;
-        let productContainerDiv = productBtnDiv.parentElement;
-        let productImage = productContainerDiv.querySelector('.products-image').src;
-        let productDetails = productContainerDiv.querySelector('.title').innerHTML;
-        console.log(productDetails);
-    })
-}
+
+/**
+ * Invoking getProduct() function
+ */
+ getProduct(productArray);
 /**
  * ON BUY BUTTON CLICK EVENT
  */
-function onItemClick(item) {
-    alert('You clicked me!')
+ function itemClicked(e){
+    let productBtnDiv = e.target.parentElement;
+    let productContainerDiv = productBtnDiv.parentElement;
+    let productImage = productContainerDiv.querySelector('.products-image').src;
+    let productTitle = productContainerDiv.querySelector('.title').innerHTML;
+    let productPrice = productContainerDiv.querySelector('.price').innerHTML;
+    let productDescription = productContainerDiv.querySelector('.description').innerHTML;
+
+    /**
+     * Process price
+     */
+    let strLen = productPrice.length;
+    let price = parseInt(productPrice.substring(1, strLen));
+
+    /**
+     * ITEM object
+     */
+    let itemObject = {
+        image: productImage,
+        title: productTitle,
+        price: price,
+        description: productDescription
+    };
+    let cart = new ShoppingCart(itemObject);
+    
+    return cart;
 }
+function getProduct(prodArr){
+for(let item of prodArr){ 
+    item.addEventListener('click', itemClicked);
+}
+}
+
 function addToCart(){
 
 }
@@ -49,12 +86,13 @@ function addToCart(){
  * CART ITEMS
  */
 function getCartItems(){
-    let cartContent = ShoppingCart.displayCart();
-    console.log(typeof(cartContent));
     clearMainContent();
     let mainCartItems = document.createElement('div');
+   
     mainCartItems.innerText = cartContent;
-
+    itemImg.src = './images/feather_duvet.jpg';
+    //Appending
+    mainCartItems.appendChild(itemImg);
     //Append mainCartItemsto the main.
     main.appendChild(mainCartItems);
 }
